760990f423f2b0c449d609502c617630823045b3
HailangSun <hailang.sun@igg.com>
2023-03-13 20:29:13 +0800

    add ilruntime domain

---
+++ Assets/Scripts/HotFix_Project~/HotFix_Project/HotFix_Project/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HotFix_Project
+{
+    public class HelloWorld
+    {
+        public static void StaticTestMethod()
+        {
+            UnityEngine.Debug.Log($"Hello World! TestMethod");
+        }
+        public static void StaticTestMethod(int param)
+        {
+            UnityEngine.Debug.Log($"Hello World! TestMethod : {param}");
+        }
+    }
+}
